0f04a4212029afc707e9ccc066f883c8c9f326b6
Strauß <StrausVa@fhhnet.stadt.hamburg.de>
2021-05-21 15:30:38 +0200

    Script welches 200 random Bilder in den Warenkorb legen soll, Server ist mit 1 Anfrage pro Sekunde nicht klargekommen... auf 5 Sekunden erhöht aber noch nicht getestet

---
+++ crawler.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Dies erzeugt eine Variable die immer einen neuen wert zwischen 0-32767 erzeugt
+RANDOM=$$
+
+# Wir setzen die $response auf 0
+response=0 
+
+# Erzeugen einer tmpFile um die Response der Request zwischenzuspeichern 
+# Die Response der Request ist die Anzahl der Items im Warenkorb
+touch tmpFile
+
+# Bis die Anzahl der Items 200 ist wird versucht eine Random id in den Warenkorn zu legen
+until [ $response == 200 ] 
+do
+    curl -o .\tmpFile "https://fotoarchiv-stadtarchiv.kiel.de/ax_Downd.FAU?sid=8D8F20621&dm=1&qpos=${RANDOM}&erg=H"
+    # Die Response wind in tmpfile gespeichert
+    response=`cat .tmpFile`
+    # Konsolenoutput der Response (meißtens ein langer Fehler, das das item nicht existiert, sonst die Anzahl der Items im Warenkorb)
+    echo $response
+    # Sleepfunktion um den Server nicht zu überlasten (auch bei einer Sekunde schmiert der Server ab... als nächstes 5s ausprobieren)
+    sleep 1
+done
+
+# Den fertigen Warenkorb kann man dann unter https://fotoarchiv-stadtarchiv.kiel.de/fl_dload.FAU?sid=8D8F20621&dm=1 herunterladen
