90abab3e7ab487feb9a79e28772a0ca2e41c91b4
Strauß <StrausVa@fhhnet.stadt.hamburg.de>
2021-05-31 08:22:33 +0200

    Sleeptime => 5 sec

--- crawler.sh
+++ crawler.sh
@@ -8,19 +8,18 @@ response=0
 
 # Erzeugen einer tmpFile um die Response der Request zwischenzuspeichern 
 # Die Response der Request ist die Anzahl der Items im Warenkorb
-touch tmpFile
 
 # Bis die Anzahl der Items 200 ist wird versucht eine Random id in den Warenkorn zu legen
 until [ $response == 200 ] 
 do
     # Die Response von curl wind in tmpfile gespeichert
-    curl -o .\tmpFile "https://fotoarchiv-stadtarchiv.kiel.de/ax_Downd.FAU?sid=8D8F20621&dm=1&qpos=${RANDOM}&erg=H"
+    curl -o ./tmpFile "https://fotoarchiv-stadtarchiv.kiel.de/ax_Downd.FAU?sid=A9DBE4851&dm=1&qpos=${RANDOM}&erg=H"
     # Und in unsere Variable geladen
-    response=`cat .tmpFile`
+    response=`cat ./tmpFile`
     # Konsolenoutput der Response (meißtens ein langer Fehler, dass das Item nicht existiert, sonst die Anzahl der Items im Warenkorb)
     echo $response
     # Sleepfunktion um den Server nicht zu überlasten (auch bei einer Sekunde schmiert der Server ab... als nächstes 5s ausprobieren)
-    sleep 1
+    sleep 5
 done
 
-# Den fertigen Warenkorb kann man dann unter https://fotoarchiv-stadtarchiv.kiel.de/fl_dload.FAU?sid=8D8F20621&dm=1 herunterladen
+# Den fertigen Warenkorb kann man dann unter https://fotoarchiv-stadtarchiv.kiel.de/fl_dload.FAU?sid=A9DBE4851&dm=1 herunterladen
